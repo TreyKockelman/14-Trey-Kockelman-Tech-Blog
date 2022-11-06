26b19d2c536b459a704386b9b57b3a6a4404d20e
TreyKockelman <kockelmant@gmail.com>
2022-11-06 16:03:14 -0600

    added default homepage for render on open in railway

--- server.js
+++ server.js
@@ -32,6 +32,8 @@ app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.get('/', (req, res) => res.sendFile("/views/homepage.handlebars"));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
