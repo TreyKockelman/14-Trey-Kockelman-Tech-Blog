77b48957de7e72d3eacbb182e43b833344c3c812
TreyKockelman <kockelmant@gmail.com>
2022-11-06 16:09:56 -0600

    added default load screen

--- server.js
+++ server.js
@@ -32,11 +32,13 @@ app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.get('/', (req, res) => res.sendFile("/views/homepage.handlebars"));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/views/homepage.handlebars'))
+);
 
 app.use(routes);
 
